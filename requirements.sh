6fc934641c7387a66a9dff90221481b19fdcbeb6
MkUnofficialUser <165890073+MkUnofficialUser@users.noreply.github.com>
2024-12-24 16:20:12 +0530

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,16 @@
+apt update -y
+pkg update -y
+apt upgrade -y
+pkg upgrade -y
+pkg install git -y
+pip install requests
+pkg install nodejs -y
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install python-pip -y
+pkg install libjpeg-turbo zlib
+pip install pillow
+pip install qrcode
+pip install requests
+pip install colorama
